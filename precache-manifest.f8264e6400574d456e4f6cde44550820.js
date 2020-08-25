4f3f79b01d3f293f5eb6637cc6129152ef090cde
strivert <strivert@email.com>
2020-08-25 15:05:29 +0400

    Updates

---
+++ precache-manifest.f8264e6400574d456e4f6cde44550820.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d644ec749157a90eb3a5ebe5e2f9e6a2",
+    "url": "/Reefolio/index.html"
+  },
+  {
+    "revision": "ff66d02e8d7cbdfff532",
+    "url": "/Reefolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "56ff2404da0a5066ddd2",
+    "url": "/Reefolio/static/js/2.4287e797.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Reefolio/static/js/2.4287e797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff66d02e8d7cbdfff532",
+    "url": "/Reefolio/static/js/main.d9fdb787.chunk.js"
+  },
+  {
+    "revision": "5811bb4ca8afccc867ff",
+    "url": "/Reefolio/static/js/runtime-main.21594beb.js"
+  }
+]);
